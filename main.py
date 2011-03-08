0eb883cc6a4ec371bfbb17a71fe00e41fc1f38c2
Robert Stewart <bstewart.ny@gmail.com>
2011-03-08 09:07:37 -0500

    Fixed python problem

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 
+from google.appengine.dist import use_library
+use_library('django', '0.96')
+
 from google.appengine.ext import webapp
-from google.appengine.ext.webapp import util
 from google.appengine.ext.webapp import template
-from google.appengine.api import mail
+from google.appengine.ext.webapp import util
+#from google.appengine.api import mail
 
 import os
 
@@ -17,21 +20,7 @@ class PixelLifeHandler(webapp.RequestHandler):
        
         self.response.out.write(template.render(path, {}))
         
-class emailHandler(webapp.RequestHandler):
-    def post(self):
-        email=self.request.get("email")
-        
-        if mail.is_email_valid(email):
-            subject=self.request.get("topic")
-            body=self.request.get("message")
-            name=self.request.get("name")
-            body=body+"\n\n"+name
-            # send feedback email
-            mail.send_mail(email, "feedback@omegamuse.com", subject,body)    
-            # send confirmation email
-            mail.send_mail("feedback@omegamuse.com",email,"Feedback received","Thank you for your feedback!  We will review your email and get back to you as soon as possible.  Cheers, OmegaMuse Team.")
-            
-        self.redirect('/pixellife/')
+ 
         
 def main():
     application = webapp.WSGIApplication([('/',MainHandler),
@@ -45,8 +34,6 @@ def main():
                                             ('/pixellife/ipad',PixelLifeHandler),
                                             ('/pixellife/ipad.html',PixelLifeHandler),
                                             ('/pixellife/iPad',PixelLifeHandler),
-                                            ('/email',emailHandler),
-                                            ('/pixellife/email',emailHandler),
                                             ('/pixellife/iPad.html',PixelLifeHandler)],debug=True)
     util.run_wsgi_app(application)
 
